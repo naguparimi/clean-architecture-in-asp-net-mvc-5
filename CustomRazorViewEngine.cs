687befa9f60adb6e0388f0b557b2f2d5bfb85037
naganjaneya.raju <naganjaneya.raju@birdres.com>
2019-07-30 15:45:08 +0530

    import package

--- CustomRazorViewEngine.cs
+++ CustomRazorViewEngine.cs
@@ -3,7 +3,7 @@ using System.Web.Mvc;
 
 namespace CleanArchitectureAspNetMvc5
 {
-public class CustomRazorViewEngine : CustomRazorViewEngine
+public class CustomRazorViewEngine : RazorViewEngine
 {
     public CustomRazorViewEngine()
     {
