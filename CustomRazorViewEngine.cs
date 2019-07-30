c577c897ba02e7188a87169424ac9ec0a006477b
naganjaneya.raju <naganjaneya.raju@birdres.com>
2019-07-30 15:30:54 +0530

    changing the razorviewengine

--- CustomRazorViewEngine.cs
+++ CustomRazorViewEngine.cs
@@ -3,7 +3,7 @@ using System.Web.Mvc;
 
 namespace CleanArchitectureAspNetMvc5
 {
-public class CustomRazorViewEngine : RazorViewEngine
+public class CustomRazorViewEngine : CustomRazorViewEngine
 {
     public CustomRazorViewEngine()
     {
